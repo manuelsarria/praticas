03768c4795afafff6147aacf1794ce68b9fd7b53
manuelsarria <40769750+manuelsarria@users.noreply.github.com>
2019-06-21 19:23:40 -0500

    first commit pratica ionic

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'action Sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert',
+    },
+    {
+      icon: 'beaker',
+      name: 'Avatar',
+      redirectTo: '/avatar',
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Botones y router',
+      redirectTo: '/botones',
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+
+}
