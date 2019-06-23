d841c70f4215f02f6323b182f6504c57343b8516
manuelsarria <40769750+manuelsarria@users.noreply.github.com>
2019-06-23 16:37:53 -0500

    implement menu options second part

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Componente } from '../../interfaces/interfaces';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,98 +11,21 @@ import { MenuController } from '@ionic/angular';
 })
 export class InicioPage implements OnInit {
 
-  componentes: Componente[] = [
-    {
-      icon: 'logo-whatsapp',
-      name: 'Alert',
-      redirectTo: '/alert',
-    },
-    {
-      icon: 'american-football',
-      name: 'action Sheet',
-      redirectTo: '/action-sheet',
-    },
-    {
-      icon: 'appstore',
-      name: 'Alert',
-      redirectTo: '/alert',
-    },
-    {
-      icon: 'beaker',
-      name: 'Avatar',
-      redirectTo: '/avatar',
-    },
-    {
-      icon: 'radio-button-on',
-      name: 'buttons and router',
-      redirectTo: '/botones',
-    },
-    {
-      icon: 'cash',
-      name: 'card',
-      redirectTo: '/card',
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'check',
-      redirectTo: '/check',
-    },
-    {
-      icon: 'calendar',
-      name: 'dateTime',
-      redirectTo: '/date-time',
-    },
-    {
-      icon: 'body',
-      name: 'fab',
-      redirectTo: '/fab',
-    },
-    {
-      icon: 'grid',
-      name: 'grid',
-      redirectTo: '/grid',
-    },
-    {
-      icon: 'infinite',
-      name: 'infinite scroll',
-      redirectTo: '/infinite-scroll',
-    },
-    {
-      icon: 'hammer',
-      name: 'input ',
-      redirectTo: '/input',
-    },
-    {
-      icon: 'hammer',
-      name: 'list ',
-      redirectTo: '/list',
-    },
-    {
-      icon: 'reorder',
-      name: 'list reorder',
-      redirectTo: '/list-reorder',
-    },
-    {
-      icon: 'refresh-circle',
-      name: 'loading',
-      redirectTo: '/loading',
-    },
-  ];
+  componentes: Observable<Componente[]>;
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor(private menuCtrl: MenuController,
+              private dataService: DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
   }
 
   toggleMenu() {
+
     this.menuCtrl.toggle();
+
   }
 
 }
 
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
 
-}
