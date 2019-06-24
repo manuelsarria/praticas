208851445a624b718f18b326c19cf15ae010b43a
manuelsarria <40769750+manuelsarria@users.noreply.github.com>
2019-06-23 20:33:09 -0500

    cambios modal info

---
+++ src/app/pages/modal/modal.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ValueAccessor } from '@ionic/angular/dist/directives/control-value-accessors/value-accessor';
+import { ModalInfoPage } from '../modal-info/modal-info.page';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+  }
+
+  async showModal() {
+
+    const modal = await this.modalCtrl.create({
+      component: ModalInfoPage,
+      componentProps: {
+        nombre: 'manuel',
+        pais: 'panama'
+      }
+    });
+    await modal.present();
+
+  }
+
+}
