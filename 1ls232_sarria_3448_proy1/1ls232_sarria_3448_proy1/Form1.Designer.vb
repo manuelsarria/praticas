f695ddb883fd4b84d290536b2433bdf881b02616
manuelsarria <40769750+manuelsarria@users.noreply.github.com>
2019-10-14 15:35:41 -0500

    prubas de estudio . net

---
+++ 1ls232_sarria_3448_proy1/1ls232_sarria_3448_proy1/Form1.Designer.vb
@@ -0,0 +1,399 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.i_result = New System.Windows.Forms.TextBox()
+        Me.itxt6 = New System.Windows.Forms.TextBox()
+        Me.itxt5 = New System.Windows.Forms.TextBox()
+        Me.itxt4 = New System.Windows.Forms.TextBox()
+        Me.itxt3 = New System.Windows.Forms.TextBox()
+        Me.itxt2 = New System.Windows.Forms.TextBox()
+        Me.itxt1 = New System.Windows.Forms.TextBox()
+        Me.itxt0 = New System.Windows.Forms.TextBox()
+        Me.p_result = New System.Windows.Forms.TextBox()
+        Me.ptxt6 = New System.Windows.Forms.TextBox()
+        Me.ptxt5 = New System.Windows.Forms.TextBox()
+        Me.ptxt4 = New System.Windows.Forms.TextBox()
+        Me.ptxt3 = New System.Windows.Forms.TextBox()
+        Me.ptxt2 = New System.Windows.Forms.TextBox()
+        Me.ptxt1 = New System.Windows.Forms.TextBox()
+        Me.ptxt0 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_iniciar = New System.Windows.Forms.Button()
+        Me.txt_tiempo = New System.Windows.Forms.TextBox()
+        Me.btn_resolver = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.result_parcial = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btn_dividir = New System.Windows.Forms.Button()
+        Me.btn_multiplicar = New System.Windows.Forms.Button()
+        Me.btn_resta = New System.Windows.Forms.Button()
+        Me.btn_suma = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'i_result
+        '
+        Me.i_result.Location = New System.Drawing.Point(252, 12)
+        Me.i_result.Name = "i_result"
+        Me.i_result.Size = New System.Drawing.Size(30, 20)
+        Me.i_result.TabIndex = 1
+        '
+        'itxt6
+        '
+        Me.itxt6.Location = New System.Drawing.Point(201, 12)
+        Me.itxt6.Name = "itxt6"
+        Me.itxt6.Size = New System.Drawing.Size(22, 20)
+        Me.itxt6.TabIndex = 2
+        '
+        'itxt5
+        '
+        Me.itxt5.Location = New System.Drawing.Point(173, 12)
+        Me.itxt5.Name = "itxt5"
+        Me.itxt5.Size = New System.Drawing.Size(22, 20)
+        Me.itxt5.TabIndex = 3
+        '
+        'itxt4
+        '
+        Me.itxt4.Location = New System.Drawing.Point(145, 12)
+        Me.itxt4.Name = "itxt4"
+        Me.itxt4.Size = New System.Drawing.Size(22, 20)
+        Me.itxt4.TabIndex = 4
+        '
+        'itxt3
+        '
+        Me.itxt3.Location = New System.Drawing.Point(117, 12)
+        Me.itxt3.Name = "itxt3"
+        Me.itxt3.Size = New System.Drawing.Size(22, 20)
+        Me.itxt3.TabIndex = 5
+        '
+        'itxt2
+        '
+        Me.itxt2.Location = New System.Drawing.Point(89, 12)
+        Me.itxt2.Name = "itxt2"
+        Me.itxt2.Size = New System.Drawing.Size(22, 20)
+        Me.itxt2.TabIndex = 6
+        '
+        'itxt1
+        '
+        Me.itxt1.Location = New System.Drawing.Point(61, 12)
+        Me.itxt1.Name = "itxt1"
+        Me.itxt1.Size = New System.Drawing.Size(22, 20)
+        Me.itxt1.TabIndex = 7
+        '
+        'itxt0
+        '
+        Me.itxt0.Location = New System.Drawing.Point(33, 12)
+        Me.itxt0.Name = "itxt0"
+        Me.itxt0.Size = New System.Drawing.Size(22, 20)
+        Me.itxt0.TabIndex = 8
+        '
+        'p_result
+        '
+        Me.p_result.Location = New System.Drawing.Point(252, 50)
+        Me.p_result.Name = "p_result"
+        Me.p_result.Size = New System.Drawing.Size(30, 20)
+        Me.p_result.TabIndex = 9
+        '
+        'ptxt6
+        '
+        Me.ptxt6.Location = New System.Drawing.Point(201, 50)
+        Me.ptxt6.Name = "ptxt6"
+        Me.ptxt6.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt6.TabIndex = 10
+        '
+        'ptxt5
+        '
+        Me.ptxt5.Location = New System.Drawing.Point(173, 50)
+        Me.ptxt5.Name = "ptxt5"
+        Me.ptxt5.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt5.TabIndex = 11
+        '
+        'ptxt4
+        '
+        Me.ptxt4.Location = New System.Drawing.Point(145, 50)
+        Me.ptxt4.Name = "ptxt4"
+        Me.ptxt4.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt4.TabIndex = 12
+        '
+        'ptxt3
+        '
+        Me.ptxt3.Location = New System.Drawing.Point(117, 50)
+        Me.ptxt3.Name = "ptxt3"
+        Me.ptxt3.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt3.TabIndex = 13
+        '
+        'ptxt2
+        '
+        Me.ptxt2.Location = New System.Drawing.Point(89, 50)
+        Me.ptxt2.Name = "ptxt2"
+        Me.ptxt2.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt2.TabIndex = 14
+        '
+        'ptxt1
+        '
+        Me.ptxt1.Location = New System.Drawing.Point(61, 50)
+        Me.ptxt1.Name = "ptxt1"
+        Me.ptxt1.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt1.TabIndex = 15
+        '
+        'ptxt0
+        '
+        Me.ptxt0.Location = New System.Drawing.Point(33, 50)
+        Me.ptxt0.Name = "ptxt0"
+        Me.ptxt0.Size = New System.Drawing.Size(22, 20)
+        Me.ptxt0.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(229, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 25
+        Me.Label1.Text = "="
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "="
+        '
+        'btn_iniciar
+        '
+        Me.btn_iniciar.Location = New System.Drawing.Point(302, 10)
+        Me.btn_iniciar.Name = "btn_iniciar"
+        Me.btn_iniciar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_iniciar.TabIndex = 27
+        Me.btn_iniciar.Text = "Iniciar"
+        Me.btn_iniciar.UseVisualStyleBackColor = True
+        '
+        'txt_tiempo
+        '
+        Me.txt_tiempo.Location = New System.Drawing.Point(452, 10)
+        Me.txt_tiempo.Name = "txt_tiempo"
+        Me.txt_tiempo.Size = New System.Drawing.Size(40, 20)
+        Me.txt_tiempo.TabIndex = 28
+        '
+        'btn_resolver
+        '
+        Me.btn_resolver.Location = New System.Drawing.Point(302, 50)
+        Me.btn_resolver.Name = "btn_resolver"
+        Me.btn_resolver.Size = New System.Drawing.Size(75, 23)
+        Me.btn_resolver.TabIndex = 29
+        Me.btn_resolver.Text = "Resolver"
+        Me.btn_resolver.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(397, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Tiempo ="
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(21, 96)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 31
+        Me.Label4.Text = "Numeros"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(18, 126)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 13)
+        Me.Label5.TabIndex = 32
+        Me.Label5.Text = "Operadores"
+        '
+        'result_parcial
+        '
+        Me.result_parcial.Location = New System.Drawing.Point(252, 93)
+        Me.result_parcial.Name = "result_parcial"
+        Me.result_parcial.Size = New System.Drawing.Size(30, 20)
+        Me.result_parcial.TabIndex = 33
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(25, 23)
+        Me.Button1.TabIndex = 34
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(179, 91)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(25, 23)
+        Me.Button2.TabIndex = 35
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(148, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(25, 23)
+        Me.Button3.TabIndex = 36
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(117, 91)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(25, 23)
+        Me.Button4.TabIndex = 37
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'btn_dividir
+        '
+        Me.btn_dividir.Location = New System.Drawing.Point(179, 120)
+        Me.btn_dividir.Name = "btn_dividir"
+        Me.btn_dividir.Size = New System.Drawing.Size(25, 23)
+        Me.btn_dividir.TabIndex = 38
+        Me.btn_dividir.Text = "/"
+        Me.btn_dividir.UseVisualStyleBackColor = True
+        '
+        'btn_multiplicar
+        '
+        Me.btn_multiplicar.Location = New System.Drawing.Point(148, 120)
+        Me.btn_multiplicar.Name = "btn_multiplicar"
+        Me.btn_multiplicar.Size = New System.Drawing.Size(25, 23)
+        Me.btn_multiplicar.TabIndex = 39
+        Me.btn_multiplicar.Text = "*"
+        Me.btn_multiplicar.UseVisualStyleBackColor = True
+        '
+        'btn_resta
+        '
+        Me.btn_resta.Location = New System.Drawing.Point(117, 120)
+        Me.btn_resta.Name = "btn_resta"
+        Me.btn_resta.Size = New System.Drawing.Size(25, 23)
+        Me.btn_resta.TabIndex = 40
+        Me.btn_resta.Text = "-"
+        Me.btn_resta.UseVisualStyleBackColor = True
+        '
+        'btn_suma
+        '
+        Me.btn_suma.Location = New System.Drawing.Point(86, 120)
+        Me.btn_suma.Name = "btn_suma"
+        Me.btn_suma.Size = New System.Drawing.Size(25, 23)
+        Me.btn_suma.TabIndex = 41
+        Me.btn_suma.Text = "+"
+        Me.btn_suma.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(669, 248)
+        Me.Controls.Add(Me.btn_suma)
+        Me.Controls.Add(Me.btn_resta)
+        Me.Controls.Add(Me.btn_multiplicar)
+        Me.Controls.Add(Me.btn_dividir)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.result_parcial)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btn_resolver)
+        Me.Controls.Add(Me.txt_tiempo)
+        Me.Controls.Add(Me.btn_iniciar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ptxt0)
+        Me.Controls.Add(Me.ptxt1)
+        Me.Controls.Add(Me.ptxt2)
+        Me.Controls.Add(Me.ptxt3)
+        Me.Controls.Add(Me.ptxt4)
+        Me.Controls.Add(Me.ptxt5)
+        Me.Controls.Add(Me.ptxt6)
+        Me.Controls.Add(Me.p_result)
+        Me.Controls.Add(Me.itxt0)
+        Me.Controls.Add(Me.itxt1)
+        Me.Controls.Add(Me.itxt2)
+        Me.Controls.Add(Me.itxt3)
+        Me.Controls.Add(Me.itxt4)
+        Me.Controls.Add(Me.itxt5)
+        Me.Controls.Add(Me.itxt6)
+        Me.Controls.Add(Me.i_result)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents i_result As System.Windows.Forms.TextBox
+    Friend WithEvents itxt6 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt5 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt4 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt3 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt2 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt1 As System.Windows.Forms.TextBox
+    Friend WithEvents itxt0 As System.Windows.Forms.TextBox
+    Friend WithEvents p_result As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt6 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt5 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt4 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt3 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt2 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt1 As System.Windows.Forms.TextBox
+    Friend WithEvents ptxt0 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btn_iniciar As System.Windows.Forms.Button
+    Friend WithEvents txt_tiempo As System.Windows.Forms.TextBox
+    Friend WithEvents btn_resolver As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents result_parcial As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents btn_dividir As System.Windows.Forms.Button
+    Friend WithEvents btn_multiplicar As System.Windows.Forms.Button
+    Friend WithEvents btn_resta As System.Windows.Forms.Button
+    Friend WithEvents btn_suma As System.Windows.Forms.Button
+
+End Class
---
+++ 1ls232_sarria_3448_proy1/1ls232_sarria_3448_proy1/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    Private Sub FlowLayoutPanel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs)
+
+    End Sub
+
+
+    Public Sub btn_iniciar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_iniciar.Click
+        Dim rnd As System.Random = New System.Random()
+        ' asignar numeros
+        itxt0.Text = rnd.Next(1, 10)
+        itxt2.Text = rnd.Next(1, 10)
+        itxt4.Text = rnd.Next(1, 10)
+        itxt6.Text = rnd.Next(1, 10)
+        ' asignar operador
+
+
+        itxt1.Text = generar_operador()
+        itxt3.Text = generar_operador()
+        itxt5.Text = generar_operador()
+
+        Dim salida As Integer
+
+        salida = generar_resultado(CInt(itxt0.Text), CInt(itxt2.Text), itxt1.Text)
+        salida = generar_resultado(salida, CInt(itxt4.Text), itxt3.Text)
+        salida = generar_resultado(salida, CInt(itxt6.Text), itxt5.Text)
+
+        i_result.Text = salida
+
+        ' MsgBox(salida)
+
+    End Sub
+
+    Public Function generar_operador()
+        Dim operador(3) As String
+
+        operador(0) = "+"
+        operador(1) = "-"
+        operador(2) = "*"
+        operador(3) = "/"
+
+        Return operador(CInt(Int((4 * Rnd()))))
+    End Function
+
+    Public Function generar_resultado(ByVal x As Integer,
+                                      ByVal y As Integer,
+                                      ByVal operador As String)
+        Select Case operador
+            Case "+"
+                Return x + y
+            Case "-"
+                Return x - y
+            Case "*"
+                Return CInt(x * y)
+            Case "/"
+                Return CInt(x / y)
+            Case Else
+                Return "exit"
+        End Select
+
+    End Function
+
+    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
