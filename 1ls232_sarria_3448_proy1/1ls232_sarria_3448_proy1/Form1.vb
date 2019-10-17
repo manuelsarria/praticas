7b3f98015d7e36b4334e3e7c46546ef1bcb20161
manuelsarria <40769750+manuelsarria@users.noreply.github.com>
2019-10-17 11:22:47 -0500

    codigo de estudio final
    falta implementación del tag que se guarde cmobntra la posicion

--- 1ls232_sarria_3448_proy1/1ls232_sarria_3448_proy1/Form1.vb
+++ 1ls232_sarria_3448_proy1/1ls232_sarria_3448_proy1/Form1.vb
@@ -1,4 +1,62 @@
 ﻿Public Class Form1
+    Dim vtxt_texto(6) As TextBox
+    Dim vtxt_num(6) As TextBox
+    Dim vbtn_num(3) As Button
+    Dim vbtn_oper(3) As Button
+
+    Public Sub New()
+
+        ' Llamada necesaria para el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        Dim i As Integer
+        Dim rnd As System.Random = New System.Random()
+        Dim txt_texto As TextBox
+        Dim txt_num As TextBox
+        Dim btn_num As Button
+        Dim btn_oper As Button
+
+        For i = 0 To 6
+            txt_texto = New TextBox
+            txt_texto.Size = New Size(22, 20)
+            txt_texto.Location = New Point(33 + (txt_texto.Width + 6) * i, 12)
+            'txt_texto.Text = rnd.Next(1, 10)
+            Controls.Add(txt_texto)
+            vtxt_texto(i) = txt_texto
+
+            txt_num = New TextBox
+            txt_num.Size = New Size(22, 20)
+            txt_num.Location = New Point(33 + (txt_num.Width + 6) * i, 43)
+            'txt_texto.Text = rnd.Next(1, 10)
+            Controls.Add(txt_num)
+            vtxt_num(i) = txt_num
+        Next
+
+        For i = 0 To 3
+            btn_num = New Button
+            btn_num.Size = New Size(22, 20)
+            btn_num.Location = New Point(75 + (btn_num.Width + 6) * i, 74)
+            btn_num.Tag = 0
+            Controls.Add(btn_num)
+            vbtn_num(i) = btn_num
+            AddHandler btn_num.Click, AddressOf btn_name_click
+
+            btn_oper = New Button
+            btn_oper.Size = New Size(22, 20)
+            btn_oper.Location = New Point(75 + (btn_oper.Width + 6) * i, 105)
+            btn_oper.Tag = 0
+            btn_oper.Enabled = False
+            Controls.Add(btn_oper)
+            vbtn_oper(i) = btn_oper
+            AddHandler btn_oper.Click, AddressOf btn_oper_click
+        Next
+
+        txt_indice.Text = 0
+
+    End Sub
+
+    Dim valores(3) As Integer
 
     Private Sub FlowLayoutPanel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs)
 
@@ -7,29 +65,79 @@
 
     Public Sub btn_iniciar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_iniciar.Click
         Dim rnd As System.Random = New System.Random()
-        ' asignar numeros
-        itxt0.Text = rnd.Next(1, 10)
-        itxt2.Text = rnd.Next(1, 10)
-        itxt4.Text = rnd.Next(1, 10)
-        itxt6.Text = rnd.Next(1, 10)
-        ' asignar operador
+        Dim i As Integer
+        Dim temp
+        Dim operador(3) As String
+        operador(0) = "+"
+        operador(1) = "-"
+        operador(2) = "*"
+        operador(3) = "/"
 
+        ' generar numeros
+        For i = 0 To 6
+            vtxt_texto(i).Text = rnd.Next(1, 10)
+            'asignar numeros de los botones
+            vbtn_num(i / 2).Text = vtxt_texto(i).Text
+            i = i + 1
+        Next
 
-        itxt1.Text = generar_operador()
-        itxt3.Text = generar_operador()
-        itxt5.Text = generar_operador()
+        'desordenar numeros
+        For i = 1 To 3
+            temp = vbtn_num(0).Text
+            vbtn_num(0).Text = vbtn_num(i).Text
+            vbtn_num(i).Text = temp
+        Next
 
-        Dim salida As Integer
+        ' generar operadores
+        For i = 1 To 5
+            vtxt_texto(i).Text = generar_operador()
+            i = i + 1
+        Next
 
-        salida = generar_resultado(CInt(itxt0.Text), CInt(itxt2.Text), itxt1.Text)
-        salida = generar_resultado(salida, CInt(itxt4.Text), itxt3.Text)
-        salida = generar_resultado(salida, CInt(itxt6.Text), itxt5.Text)
+        For i = 0 To 3
+            vbtn_oper(i).Text = operador(i)
 
-        i_result.Text = salida
+        Next
 
-        ' MsgBox(salida)
+        i_result.Text = realizar_operacion(vtxt_texto)
+
 
     End Sub
+
+    Sub btn_name_click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim btn_temp As Button
+        btn_temp = CType(sender, Button)
+        vtxt_num(txt_indice.Text).Text = btn_temp.Text
+        If Val(txt_indice.Text) <= 6 Then
+            vtxt_num(Val(txt_indice.Text)).Text = btn_temp.Text
+            txt_indice.Text = Val(txt_indice.Text) + 1
+
+        End If
+        p_result.Text = realizar_operacion(vtxt_num)
+
+        For i = 0 To 3
+            vbtn_num(i).Enabled = False
+            vbtn_oper(i).Enabled = True
+        Next
+    End Sub
+
+    Sub btn_oper_click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim btn_temp As Button
+        btn_temp = CType(sender, Button)
+        vtxt_num(txt_indice.Text).Text = btn_temp.Text
+        If Val(txt_indice.Text) <= 6 Then
+            vtxt_num(Val(txt_indice.Text)).Text = btn_temp.Text
+            txt_indice.Text = Val(txt_indice.Text) + 1
+
+        End If
+        p_result.Text = realizar_operacion(vtxt_num)
+
+        For i = 0 To 3
+            vbtn_num(i).Enabled = True
+            vbtn_oper(i).Enabled = False
+        Next
+    End Sub
+
 
     Public Function generar_operador()
         Dim operador(3) As String
@@ -42,25 +150,40 @@
         Return operador(CInt(Int((4 * Rnd()))))
     End Function
 
-    Public Function generar_resultado(ByVal x As Integer,
-                                      ByVal y As Integer,
-                                      ByVal operador As String)
-        Select Case operador
-            Case "+"
-                Return x + y
-            Case "-"
-                Return x - y
-            Case "*"
-                Return CInt(x * y)
-            Case "/"
-                Return CInt(x / y)
-            Case Else
-                Return "exit"
-        End Select
+    Public Function realizar_operacion(ByVal data() As TextBox)
+
+        'llamado a la clase
+        Dim operacion As New Operacion()
+
+        Dim i As Integer
+        Dim resultado As Integer
+
+        If Not String.IsNullOrEmpty(data(0).Text) Then
+            resultado = CInt(data(i).Text)
+        End If
+
+        If Not String.IsNullOrEmpty(data(2).Text) Then
+            resultado = operacion.generar_resultado(resultado, CInt(data(2).Text), data(1).Text)
+        Else
+            Return resultado
+        End If
+
+        If Not String.IsNullOrEmpty(data(4).Text) Then
+            resultado = operacion.generar_resultado(resultado, CInt(data(4).Text), data(3).Text)
+        Else
+            Return resultado
+        End If
+
+        If Not String.IsNullOrEmpty(data(6).Text) Then
+            resultado = operacion.generar_resultado(resultado, CInt(data(6).Text), data(5).Text)
+        End If
+
+        Return resultado
 
     End Function
 
-    Private Sub Label5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label5.Click
+
+    Private Sub btn_resolver_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_resolver.Click
 
     End Sub
 End Class
---
+++ 1ls232_sarria_3448_proy1/1ls232_sarria_3448_proy1/Operacion.vb
@@ -0,0 +1,21 @@
+﻿Public Class Operacion
+
+    Public Function generar_resultado(ByVal x As Integer,
+                                      ByVal y As Integer,
+                                      ByVal operador As String)
+        Select Case operador
+            Case "+"
+                Return x + y
+            Case "-"
+                Return x - y
+            Case "*"
+                Return CInt(x * y)
+            Case "/"
+                Return CInt(x / y)
+            Case Else
+                Return "exit"
+        End Select
+
+    End Function
+
+End Class
